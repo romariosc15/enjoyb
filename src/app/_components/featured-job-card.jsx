d4986ad4e8e37de1b2539f1ead8f607665d2cbbe
romarioscbl <romario.sarmiento@buildinglink.com>
2023-10-19 00:37:17 -0500

    Modify featured jobs

--- src/app/_components/featured-job-card.jsx
+++ src/app/_components/featured-job-card.jsx
@@ -2,36 +2,38 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { MdOutlineWatchLater, MdCalendarMonth, MdLocationOn } from 'react-icons/md';
 export default function FeaturedJobCard(props) {
-    return (
-      <div className='border border-default rounded-md px-5 py-10'>
-        <div className='flex flex-row space-x-4'>
-          <Image
-            src={`/images/companies/${props.job.logo}`}
-            width={40}
-            height={40}
-            alt={props.job.name}
-          />
-          <div className='text-left'>
-            <p className='text-title-muted text-xs'>{ props.job.company }</p>
-            <Link href={`/jobs/detail/${props.job.id}`} className='text-link-primary font-bold'>
-                { props.job.name }
-            </Link>
-          </div>
-        </div>
-        <p className='text-description-primary text-sm mt-4'>{ props.job.description }</p>
-        <div className='space-x-2 mt-4 text-span-primary font-medium'>
-          <span className='text-xs bg-span-primary py-1 px-2'><MdLocationOn className='inline-block align-sub' size={15} /> { props.job.location }</span>
-          <span className='text-xs bg-span-primary py-1 px-2'><MdOutlineWatchLater className='inline-block align-sub' size={15} /> { props.job.typeOfContract }</span>
-          <span className='text-xs bg-span-primary py-1 px-2'><MdCalendarMonth className='inline-block align-sub' size={15} /> { props.job.date }</span>
-        </div>
-        <div className='mt-10 flex flex-row justify-between items-center'>
-            <span className='text-title-primary font-extrabold text-normal'>
-                {`$${props.job.salaryMin} - $${props.job.salaryMax}`}
-            </span>
-            <button className='bg-button-primary text-white text-sm font-medium px-4 py-3 rounded-xl'>
-              Aplicación rápido
-            </button>
+  console.log(props.job)
+  return (
+    <div className='border border-default rounded-md px-5 py-10'>
+      <div className='flex flex-row space-x-4'>
+        <Image
+          className='w-12 h-12'
+          width={48}
+          height={48}
+          src={`https:${props.job.fields.logo.fields.file.url}`}
+          alt={props.job.fields.title}
+        />
+        <div className='text-left'>
+          <p className='text-title-muted text-xs'>{ props.job.fields.company }</p>
+          <Link href={`/jobs/detail/${props.job.sys.id}`} className='text-link-primary font-bold'>
+              { props.job.fields.title }
+          </Link>
         </div>
       </div>
-    )
+      <p className='text-description-primary text-sm mt-4'>{ props.job.fields.summary }</p>
+      <div className='space-x-2 mt-4 text-span-primary font-medium'>
+        <span className='text-xs bg-span-primary py-1 px-2'><MdLocationOn className='inline-block align-sub' size={15} /> { props.job.fields.location }</span>
+        <span className='text-xs bg-span-primary py-1 px-2'><MdOutlineWatchLater className='inline-block align-sub' size={15} /> { props.job.fields.jobType }</span>
+        <span className='text-xs bg-span-primary py-1 px-2'><MdCalendarMonth className='inline-block align-sub' size={15} /> { props.job.fields.publicationDate }</span>
+      </div>
+      <div className='mt-10 flex flex-row justify-between items-center'>
+          <span className='text-title-primary font-extrabold text-normal'>
+              {`$${props.job.fields.minimumSalary} - $${props.job.fields.maximumSalary}`}
+          </span>
+          <button className='bg-button-primary text-white text-sm font-medium px-4 py-3 rounded-xl'>
+            Aplicación rápida
+          </button>
+      </div>
+    </div>
+  )
 }
--- src/app/_components/featured-jobs.jsx
+++ src/app/_components/featured-jobs.jsx
@@ -1,22 +1,22 @@
 import FeaturedJobCard from '@/app/_components/featured-job-card'
-export default function FeaturedJobs() {
-    const data = [
-      {id: 1, logo: 'facebook.png', name: 'Frontend Developer', description: 'CSS3, HTML5, Javascript, Material UI & React', salaryMin: 5500, salaryMax: 7000, company: 'Facebook', location: 'California', typeOfContract: 'Full time', date: '07/10/2023'},
-      {id: 2, logo: 'microsoft.png', name: 'Backend Developer', description: 'Javascript, Node.js, Express & SQL', salaryMin: 5000, salaryMax: 7250, company: 'Microsoft', location: 'New York', typeOfContract: 'Part time', date: '07/10/2023'},
-      {id: 3, logo: 'google.png', name: 'Frontend Developer', description: 'CSS3, HTML5, Javascript, Tailwind & Vue', salaryMin: 5250, salaryMax: 7250, company: 'Google', location: 'San Francisco', typeOfContract: 'Full time', date: '07/10/2023'},
-      {id: 4, logo: 'airbnb.png', name: 'UX/UI Designer', description: 'Adobe XD, Figma, HTML & CSS', salaryMin: 4000, salaryMax: 6500, company: 'Airbnb', location: 'San Francisco', typeOfContract: 'Full time', date: '07/10/2023'}
-    ]
-    return (
-      <div className='container mx-auto py-12'>
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-4xl font-bold text-title-primary'>Trabajos destacados</h1>
-            <p className='mt-3 text-description-primary'>
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-            </p>
-        </div>
-        <div className='mt-8 grid grid-cols-4 gap-5'>
-          {data.map((job) => <FeaturedJobCard key={job.id} job={job} />)}
-        </div>
+import { contentful } from '@/app/_libs/contentful';
+export default async function FeaturedJobs() {
+  const response = await contentful.getEntries({
+    content_type: 'jobs'
+  });
+
+  const jobs = response.items;
+  return (
+    <div className='container mx-auto py-12'>
+      <div className='flex flex-col justify-center items-center'>
+          <h1 className='text-4xl font-bold text-title-primary'>Trabajos destacados</h1>
+          <p className='mt-3 text-description-primary'>
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          </p>
       </div>
-    )
+      <div className='mt-8 grid grid-cols-4 gap-5'>
+        {jobs.map((job) => <FeaturedJobCard key={job.sys.id} job={job} />)}
+      </div>
+    </div>
+  )
 }
