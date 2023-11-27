f39ac7a46a9ff16f40a5b2076f3642f06eb36ef3
romarioscbl <romario.sarmiento@buildinglink.com>
2023-11-27 18:31:58 -0500

    Change cards

--- src/app/_components/Skeletons/featured-job-card.jsx
+++ src/app/_components/Skeletons/featured-job-card.jsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import {Skeleton} from "@nextui-org/skeleton";
 
 export default function FeaturedJobCard(props) {
--- src/app/_components/job-card.jsx
+++ src/app/_components/job-card.jsx
@@ -3,50 +3,53 @@ import Image from 'next/image'
 import { MdLocationOn, MdOutlineBrokenImage } from 'react-icons/md';
 import { FaMoneyBillWave } from "react-icons/fa6";
 import {Button} from "@nextui-org/button";
+import {Card, CardBody} from "@nextui-org/card";
 
 export default function JobCard(props) {
     return (
-        <div className='border border-default rounded-md px-4 xl:px-6 py-4'>
-            <div className='grid grid-cols-5 xl:flex xl:flex-row items-center gap-y-4'>
-                {
-                    'logo' in props.job.fields ? 
-                        <Image
-                            className='w-12 h-12'
-                            width={48}
-                            height={48}
-                            src={`https:${props.job.fields.logo.fields.file.url}`}
-                            alt={props.job.fields.title}
-                        />
-                    : <MdOutlineBrokenImage className='w-12 h-12' />
-                }
-                <div className='xl:ml-4 col-span-4 flex flex-col'>
-                    <span className='text-xs w-fit font-medium bg-green-100 text-green-700 py-1 px-3 rounded-md'>{ props.job.fields.jobType.fields.name }</span>
-                    <Link href={`/jobs/detail/${props.job.sys.id}`} className='text-link-primary text-lg font-bold mt-1'>
-                        { props.job.fields.title }
-                    </Link>
-                    <div className='space-x-4 font-medium'>
-                        <span className='text-description-alternative text-xs'>{ props.job.fields.company }</span>
-                        <span className='text-description-alternative text-xs'><MdLocationOn className='inline-block align-sub' size={15} />{ props.job.fields.location }</span>
-                        <span className='text-description-alternative text-xs'>{ props.job.fields.publicationDate }</span>
+        <Card className='px-2 py-1'>
+            <CardBody>
+                <div className='grid grid-cols-5 xl:flex xl:flex-row items-center gap-y-4'>
+                    {
+                        'logo' in props.job.fields ? 
+                            <Image
+                                className='w-12 h-12'
+                                width={48}
+                                height={48}
+                                src={`https:${props.job.fields.logo.fields.file.url}`}
+                                alt={props.job.fields.title}
+                            />
+                        : <MdOutlineBrokenImage className='w-12 h-12' />
+                    }
+                    <div className='xl:ml-4 col-span-4 flex flex-col'>
+                        <span className='text-xs w-fit font-medium bg-green-100 text-green-700 py-1 px-3 rounded-md'>{ props.job.fields.jobType.fields.name }</span>
+                        <Link href={`/jobs/detail/${props.job.sys.id}`} className='text-link-primary text-lg font-bold mt-1'>
+                            { props.job.fields.title }
+                        </Link>
+                        <div className='space-x-4 font-medium'>
+                            <span className='text-description-alternative text-xs'>{ props.job.fields.company }</span>
+                            <span className='text-description-alternative text-xs'><MdLocationOn className='inline-block align-sub' size={15} />{ props.job.fields.location }</span>
+                            <span className='text-description-alternative text-xs'>{ props.job.fields.publicationDate }</span>
+                        </div>
+                    </div>
+                    <div className='mx-auto hidden xl:flex xl:flex-row xl:items-center xl:gap-2'>
+                        <div className='rounded-full flex flex-row bg-icon-primary items-center p-1.5'>
+                            <FaMoneyBillWave className='inline-block align-sub text-white' size={20} />
+                        </div>
+                        <span className='text-title-primary font-extrabold text-xl'>
+                            {`$${props.job.fields.minimumSalary}/yr - $${props.job.fields.maximumSalary}/yr`}
+                        </span>
+                    </div>
+                    <div className='space-x-4 col-span-5 text-center'>
+                        <Button href={`/jobs/detail/${props.job.sys.id}`} as={Link} className='bg-gray-alternative text-button-primary'>
+                            Ver detalle
+                        </Button>
+                        <Button className='bg-button-primary text-white'>
+                            Aplicación rápida
+                        </Button>
                     </div>
                 </div>
-                <div className='mx-auto hidden xl:flex xl:flex-row xl:items-center xl:gap-2'>
-                    <div className='rounded-full flex flex-row bg-icon-primary items-center p-1.5'>
-                        <FaMoneyBillWave className='inline-block align-sub text-white' size={20} />
-                    </div>
-                    <span className='text-title-primary font-extrabold text-xl'>
-                        {`$${props.job.fields.minimumSalary}/yr - $${props.job.fields.maximumSalary}/yr`}
-                    </span>
-                </div>
-                <div className='space-x-4 col-span-5 text-center'>
-                    <Button href={`/jobs/detail/${props.job.sys.id}`} as={Link} className='bg-gray-alternative text-button-primary'>
-                        Ver detalle
-                    </Button>
-                    <Button className='bg-button-primary text-white'>
-                        Aplicación rápida
-                    </Button>
-                </div>
-            </div>
-        </div>
+            </CardBody>
+        </Card>
     )
 }
