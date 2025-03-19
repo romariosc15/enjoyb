efb48aac4f103a08e792b01638eaeffe3a9a7d3a
Jaime Romario Sarmiento <98931570+romariosc15@users.noreply.github.com>
2025-03-19 14:34:45 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+      }
+    },
     extend: {
       colors: {
         'title-primary': '#05264e',
