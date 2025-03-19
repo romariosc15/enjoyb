13c50b031fd7df09c9785ae5ac6a37fc25986a69
Jaime Romario Sarmiento <98931570+romariosc15@users.noreply.github.com>
2025-03-19 13:18:16 -0500

    Update footer.jsx

--- src/app/_components/footer.jsx
+++ src/app/_components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className='bg-section-primary text-white'>
             <div className="container mx-auto py-3 text-center">
-                <h1 className={`text-sm font-medium text-white`}>2023 - Desarrollado por Romario Sarmiento</h1>
+                <h1 className={`text-sm font-medium text-white`}>2025 - Desarrollado por Romario Sarmiento</h1>
             </div>
         </div>
     )
